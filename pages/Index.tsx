def4734a8775ad3c27f9dfaee47ddf5f934b7aad
Robert S <rsavian@nerdery.com>
2019-05-17 22:22:03 -0500

    sass

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -1,3 +1,31 @@
-const IndexPage = () => <h1>Testing Next.js App written in TypeScript with Jest</h1>;
+import './Index.scss';
 
-export default IndexPage;
+import React from 'react';
+import data from '../data/data.json';
+import Card from '../src/modules/Card';
+
+export default class Index extends React.Component<any, any>  {
+  static async getInitialProps () {
+    return { cards: data }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="/static/logo.png"
+               className="static-logo" alt="logo"
+          />
+        </header>
+        <div className="Grid">
+          {
+            this.props.cards.map((card) => (
+              <Card key={card.id} />
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+
+};
